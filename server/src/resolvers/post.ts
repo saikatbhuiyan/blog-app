49104a06742286e7ed51081928d6f621ec38e7c2
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-18 01:58:56 +0600

    retrive post

---
+++ server/src/resolvers/post.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query, Ctx, Arg } from "type-graphql";
+import { Post } from "../entities/Post";
+import { MyContext } from "../types";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+}
